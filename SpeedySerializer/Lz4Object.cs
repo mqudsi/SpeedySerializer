edcbcfbeba80c4a4f9511f2590534dedbb5e6daf
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-11-24 01:45:02 -0600

    New in-memory dynamic (LZ4) compression types

---
+++ SpeedySerializer/Lz4Object.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSmart.SpeedySerializer
+{
+    class Lz4Object<T>
+    {
+        private byte[] _byteStream;
+
+        public Lz4Object()
+        {
+        }
+
+        public Lz4Object(T t)
+        {
+            Box(t);
+        }
+
+        //Generic boxing
+        public void Box(T t)
+        {
+            var bytes = Encoding.UTF8.GetBytes(NetJSON.NetJSON.Serialize(t));
+            _byteStream = Lz4Net.Lz4.CompressBytes(bytes);
+        }
+
+        public void Unbox(out T t)
+        {
+            var uncompressed = Lz4Net.Lz4.DecompressBytes(_byteStream);
+            t = NetJSON.NetJSON.Deserialize<T>(Encoding.UTF8.GetString(uncompressed));
+        }
+
+        //String boxing
+        public void Box(string t)
+        {
+            var bytes = Encoding.UTF8.GetBytes(t);
+            _byteStream = Lz4Net.Lz4.CompressBytes(bytes);
+        }
+
+        public void Unbox(out string t)
+        {
+            var uncompressed = Lz4Net.Lz4.DecompressBytes(_byteStream);
+            t = Encoding.UTF8.GetString(uncompressed);
+        }
+    }
+}
