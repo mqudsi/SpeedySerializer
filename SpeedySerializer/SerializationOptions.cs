13802b00881ceb1131f29a64900f0d4f66f7ac83
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-11-30 16:03:52 -0600

    Added missing files

---
+++ SpeedySerializer/SerializationOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSmart.SpeedySerializer
+{
+    public class SerializationOptions
+    {
+        public string BaseDirectory { get; set; } = "";
+        public CompressionLevel CompressionLevel { get; set; } = CompressionLevel.Fastest;
+        public SerializationMethod Engine = SerializationMethod.NewtonSoft;
+    }
+}
